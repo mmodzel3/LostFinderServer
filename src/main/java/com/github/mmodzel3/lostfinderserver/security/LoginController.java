c9e845e50b2df3987c3c741bec3925a1f3dda9e3
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2020-10-31 21:01:30 +0100

    Added user login.

---
+++ src/main/java/com/github/mmodzel3/lostfinderserver/security/LoginController.java
@@ -0,0 +1,17 @@
+package com.github.mmodzel3.lostfinderserver.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("login")
+class LoginController {
+
+    @Autowired
+    AuthenticationService authenticationService;
+
+    @PostMapping
+    String login(@RequestParam String email, @RequestParam String password) {
+        return authenticationService.login(email, password);
+    }
+}
