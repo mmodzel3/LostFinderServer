49f5306960c8e262e8442c30afc45371d30c6e0d
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2020-10-31 15:38:17 +0100

    Initial commit

---
+++ src/test/java/com/github/mmodzel3/lostfinderserver/LostFinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mmodzel3.lostfinderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostFinderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
