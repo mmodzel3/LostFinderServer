5a4e2375ec418973fc34855dc2b9dae5a8887113
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2020-11-26 17:58:35 +0100

    Changed json date format in User, ChatMessage and ChatUserMessage.

--- src/main/java/com/github/mmodzel3/lostfinderserver/chat/ChatUserMessage.java
+++ src/main/java/com/github/mmodzel3/lostfinderserver/chat/ChatUserMessage.java
@@ -1,9 +1,15 @@
 package com.github.mmodzel3.lostfinderserver.chat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -13,5 +19,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 class ChatUserMessage {
     private String msg;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime sendDate;
 }
